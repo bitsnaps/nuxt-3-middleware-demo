795b592715bc0fe5dfaaccf359f1a529a9676716
bitsnaps <bitsnaps@yahoo.fr>
2023-11-12 16:24:24 +0100

    create a named middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <script setup>
 const isAuthenticated = useCookie('is-authenticated')
 const currentUser = useCookie('current-user')
-
-if (!isAuthenticated.value) {
-  return navigateTo('/login')
-} else if (to.params.username !== currentUser.value) { 
-  return navigateTo('/profile/' + currentUser.value)
-}
 </script>
 
 <template>
