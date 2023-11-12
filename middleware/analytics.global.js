47b6a4af8a1150e447794b88c93466769f9ef82b
bitsnaps <bitsnaps@yahoo.fr>
2023-11-12 16:53:47 +0100

    handle errors in a middleware

--- middleware/analytics.global.js
+++ middleware/analytics.global.js
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     console.log('----- Analytics middleware');
-    console.log('from: ', from);
-    console.log('to: ', to);
+    //console.log('from: ', from);
+    //console.log('to: ', to);
 })
--- middleware/setup.global.js
+++ middleware/setup.global.js
@@ -1,4 +1,20 @@
 // Global middlewares are executed by alphabetical order
-export default defineNuxtRouteMiddleware((to, from) => {    
+export default defineNuxtRouteMiddleware(async (to, from) => {    
     console.log('----- Setup Global middleware');
+
+    /* Handling an error in a middleware
+    try {
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+        return navigateTo('/profile/'+ data.user.name);
+    } catch (error) {
+        return abortNavigation("Failing at a middleware: "+ error);
+    }*/
+
+    /*/ Custom error
+    if (to.path === '/login'){
+        throw createError({
+            statusCode: 405,
+            statusMessage: 'Oh, sorry it broke!'
+        })
+    }*/
 })
